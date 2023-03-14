0421ccc8deda1ff3fc9ea300289ef7a67400aaf0
Luis Ruiz <l.ruiz5@live.com>
2023-03-14 11:24:48 -0700

    setup components for use throughout app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { AllRoutes } from "./routes/AllRoutes";
-import "./App.css";
+import { Header, Footer } from "./components";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+          <Link to="/" className="flex items-center">
+            <img src={Logo} className="h-6 mr-3 sm:h-9" alt="Bloomify Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Bloomify
+            </span>
+          </Link>
+          <div className="flex items-center relative">
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-lightbulb-fill"></span>
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
+            <Link to={"/cart"} className="text-gray-700 dark:text-white mr-5">
+              <span className="text-2xl bi bi-cart-fill relative">
+                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full">
+                  0
+                </span>
+              </span>
+            </Link>
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person-circle"></span>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
